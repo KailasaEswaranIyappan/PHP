481882806370ca84692472cd3c986b171e7e8350
Kailasa Eswaran <40649591+KailasaEswaranIyappan@users.noreply.github.com>
2018-07-21 10:22:43 +0530

    Add files via upload

---
+++ exp1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $txt1 = "PHP";
+$txt2 = "kailash";
+$x = 24;
+$y = 22;
+print "<h2>$txt1</h2>";
+print "PHP by $txt2<br>";
+print "The added output is";
+print $x + $y ;
+        ?>
+    </body>
+</html>
---
+++ exp2.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $x = "Hey there..";
+$y = 'how are u';
+echo $x;
+echo "<br>"; 
+echo $y;echo "<br>"; 
+$x = "kailash";
+var_dump($x);
+        ?>
+    </body>
+</html>
